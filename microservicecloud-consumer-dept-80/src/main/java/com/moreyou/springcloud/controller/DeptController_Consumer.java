3e0b875de6fa3784fb80aad2d2daed3573b37fd1
xbv <zxb2018smile@aliyun.com>
2019-11-12 21:36:10 +0800

    default

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/moreyou/springcloud/controller/DeptController_Consumer.java
@@ -0,0 +1,42 @@
+package com.moreyou.springcloud.controller;
+
+import com.moreyou.springcloud.entities.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.xml.crypto.Data;
+import java.util.List;
+
+@RestController
+public class DeptController_Consumer {
+
+    private static final String REST_URL_PREFIX = "http://localhost:8001";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * 使用restTemplate访问restful接口非常的简单粗暴无脑。
+     * (url, requestMap, ResponseBean.class)这三个参数分别代表
+     * REST请求地址、请求参数、HTTP响应转换被转换成的对象类型。
+     */
+    @RequestMapping(value = "/consumer/dept/add",method = RequestMethod.POST)
+    public boolean add(Dept dept){
+      return restTemplate.postForObject(REST_URL_PREFIX+"/dept/add",dept,Boolean.class);
+    }
+
+    @RequestMapping(value = "/consumer/dept/get/{id}",method = RequestMethod.GET)
+    public Dept get(@PathVariable("id") Long id){
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/get/" + id,Dept.class);
+    }
+    @SuppressWarnings("unchecked")
+    @RequestMapping(value = "/consumer/dept/list",method = RequestMethod.GET)
+    public List<Dept> findAll(){
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list",List.class);
+    }
+
+}
